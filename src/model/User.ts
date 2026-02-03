f004ed87c7df34556bb1035d3aae536006606a87
ANSH <ansh310.sharma@gmail.com>
2026-02-03 13:40:57 +0530

    first commit

---
+++ src/model/User.ts
@@ -0,0 +1,109 @@
+import mongoose, { Schema, Document} from "mongoose";
+
+
+//for interview
+
+export interface Interview extends Document {
+    interviewType: "skill" | "role"
+    skills?: string[]
+    role?: string
+
+    softSkillsScore: number
+    technicalSkillsScore: number
+    overallScore: number
+
+    createdAt: Date
+}
+
+const InterviewSchema: Schema<Interview> = new Schema({
+    interviewType:{
+        type: String,
+        enum: ["skill", "role"],
+        required: true
+    },
+    skills:{
+        type: [String],
+        default:[]
+    }
+    ,
+    role:{
+        type: String,
+        default: null
+    
+    },
+    softSkillsScore:{
+        type: Number,
+        required: true
+    },
+    technicalSkillsScore:{
+        type: Number,
+        required: true
+    },
+    overallScore:{
+        type: Number,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+},
+{_id: false}
+);
+
+//User Schema
+
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    isVerified: boolean;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    interviews: Interview[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true
+    }
+    ,
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        match: [ /^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please fill a valid email address"]
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    verifyCode:{
+        type: String,
+        required: [true, "Verification code is required"]
+    },
+    verifyCodeExpiry:{
+        type: Date,
+        required: [true, "Verification code expiry date is required"]
+    },
+    interviews:{
+        type: [InterviewSchema]
+    }
+})
+
+const UserModel =
+    (mongoose.models.User as mongoose.Model<User>) ||
+    mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
+
