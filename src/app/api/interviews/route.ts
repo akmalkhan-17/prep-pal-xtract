fd37c781f82860c08164949d624ba3aacc6e2fe8
ANSH <ansh310.sharma@gmail.com>
2026-02-14 00:24:52 +0530

    all interviews api

---
+++ src/app/api/interviews/route.ts
@@ -0,0 +1,60 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { auth } from "@/auth";
+
+export async function GET() {
+
+    await dbConnect()
+
+    try {
+    const session = await auth();
+
+    if (!session || !session.user?.id) {
+        return Response.json(
+            { success: false, message: "Unauthorized" },
+            { status: 401 }
+        );
+    }
+    const user = await UserModel.findById(session.user.id);
+
+    if (!user) {
+        return Response.json(
+            { success: false, message: "User not found" },
+            { status: 404 }
+        );
+    }
+
+    const interviews = user.interviews
+    .sort((a, b) => {
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    })
+    .map((interview) => ({
+        _id: interview._id,
+        interviewType: interview.interviewType,
+        role: interview.role,
+        softSkillsScore: interview.softSkillsScore,
+        technicalSkillsScore: interview.technicalSkillsScore,
+        overallScore: interview.overallScore,
+        totalQuestions: interview.questions?.length || 0,
+        createdAt: interview.createdAt,
+    }));
+
+    return Response.json(
+        {
+            success: true,
+            total: interviews.length,
+            interviews,
+        },
+        { status: 200 }
+    );
+
+    } catch (error) {
+        console.error("Error in /api/interviews:", error);
+
+    return Response.json(
+        { success: false, message: "Internal Server Error" },
+        { status: 500 }
+    );
+    }
+    
+}
